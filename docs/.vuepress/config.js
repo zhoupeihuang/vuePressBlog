6d07464959be26599bc1cffb9e6d962548ed565f
周佩煌 <zhoupeihuang@sinopharm.com>
2021-12-17 13:41:04 +0800

    更新路由

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,9 +10,9 @@ module.exports = {
   },
   themeConfig: {
     nav:[ // 导航栏配置
-      {text: '前端基础', link: '/accumulate/' },
-      {text: '算法题库', link: '/algorithm/'},
-      {text: '微博', link: 'https://baidu.com'}      
+      {text: '前端基础', link: '/base/' },
+      // {text: '算法题库', link: '/algorithm/'},
+      // {text: '微博', link: 'https://baidu.com'}      
     ],
     sidebar: 'auto', // 侧边栏配置
     sidebarDepth: 2, // 侧边栏显示2级
