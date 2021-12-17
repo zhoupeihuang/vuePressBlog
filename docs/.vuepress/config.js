80f27dd0da81d5598108b984340933094030ac7a
zhoupeihuang <625506370@qq.com>
2021-12-17 16:33:24 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -15,23 +15,23 @@ module.exports = {
       link: '/'
     }],
     sidebar: {
-      // 'jinjie': [{
-      //   title: '进阶',
-      //   collapsable: true, // 可折叠
-      //   children: [
-      //     '/jinjie/十分钟了解BFC',
-      //     '/jinjie/手写装饰器'
-      //   ]
-      // }],
-      '/base/': [
-        '',
-        '闭包',
-        '错误提示',
-        '防抖节流',
-        '冒泡排序',
-        '前端跨域',
-        '什么是装饰器',
-      ]
+      'jinjie': [{
+        title: '进阶',
+        collapsable: true, // 可折叠
+        children: [
+          '/jinjie/十分钟了解BFC',
+          '/jinjie/手写装饰器'
+        ]
+      }],
+//       '/base/': [
+//         '',
+//         '闭包',
+//         '错误提示',
+//         '防抖节流',
+//         '冒泡排序',
+//         '前端跨域',
+//         '什么是装饰器',
+//       ]
     },
     // { text: 'External', link: 'https://vuepress.docschina.org/default-theme-config/#%E4%B8%BB%E9%A1%B5-homepage' },
     sidebar: 'auto', // 侧边栏配置
